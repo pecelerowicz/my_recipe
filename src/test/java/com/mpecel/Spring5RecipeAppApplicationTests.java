255607c8fef7870d95687f9a41d0a39c5d9d2aa5
Michal Pecelerowicz <michal.pecelerowicz@gmail.com>
2020-01-04 13:16:30 +0100

    Initial commit

---
+++ src/test/java/com/mpecel/Spring5RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpecel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
