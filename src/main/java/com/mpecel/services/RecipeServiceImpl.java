9db419edd018d726e09f345394836b58b0fa6a86
Michal Pecelerowicz <michal.pecelerowicz@gmail.com>
2020-01-21 20:47:57 +0100

    Add Lombok and start refactoring

--- src/main/java/com/mpecel/services/RecipeServiceImpl.java
+++ src/main/java/com/mpecel/services/RecipeServiceImpl.java
@@ -2,12 +2,14 @@ package com.mpecel.services;
 
 import com.mpecel.domain.Recipe;
 import com.mpecel.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -20,6 +22,8 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in the service");
+
         Set<Recipe> recipes = new HashSet<>();
         Iterable<Recipe> iterableRecipes = recipeRepository.findAll();
         iterableRecipes.iterator().forEachRemaining(recipe -> recipes.add(recipe)); // recipes::add
