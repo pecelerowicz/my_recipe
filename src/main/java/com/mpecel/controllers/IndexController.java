1ca5cdf02f5cd5566d975316c4f38ba08d100e61
Michal Pecelerowicz <michal.pecelerowicz@gmail.com>
2020-01-21 21:27:23 +0100

    Lombok refactor 2 (except Ingredient - couses some stack exceed...)
    q
    :
    ls
    
    .

--- src/main/java/com/mpecel/controllers/IndexController.java
+++ src/main/java/com/mpecel/controllers/IndexController.java
@@ -2,12 +2,14 @@ package com.mpecel.controllers;
 
 import com.mpecel.domain.Recipe;
 import com.mpecel.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Set;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -19,6 +21,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model) {
+        log.debug("Getting index page");
 
         model.addAttribute("recipes", recipeService.getRecipes());
 
